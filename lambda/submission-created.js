db98103f583ab69a4659da6b7229f5b57c7098b6
Sasha Klein <sashafklein@gmail.com>
2021-02-18 11:29:21 -0600

    Bit more logging

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -5,6 +5,7 @@ require("dotenv").config({
 
 // For more info, check https://docs.netlify.com/functions/build-with-javascript
 module.exports.handler = async function (event) {
+  console.log("IN FUNCTION");
   const payload = JSON.parse(event.body).payload.data;
   const { HUBSPOT_FORM_ID, HUBSPOT_API_KEY, HUBSPOT_PORTAL_ID } = process.env;
 
@@ -24,7 +25,7 @@ module.exports.handler = async function (event) {
   };
 
   const url = `https://api.hsforms.com/submissions/v3/integration/submit/${HUBSPOT_PORTAL_ID}/${HUBSPOT_FORM_ID}?hapikey=${HUBSPOT_API_KEY}`;
-  console.log("HERE", url, data);
+  console.log("ABOUT TO POST", url, data);
 
   try {
     const res = await fetch(url, {
