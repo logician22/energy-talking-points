0cd84c917af9e18ebed34adf02f068261620d6f3
Sasha Klein <sashafklein@gmail.com>
2021-04-04 15:50:29 -0500

    Main issue fixed; need to check image resolution

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,11 @@ module.exports = {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+        customizeWebpackConfig: (config) => {
+          config.plugins = config.plugins.filter(
+            (plugin) => plugin.constructor.name !== "StaticQueryMapper"
+          );
+        },
       },
     },
     // {
