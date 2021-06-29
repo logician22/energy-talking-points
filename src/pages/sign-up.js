aafa1a9c5bc0ef31dc8b9f1e48f09e0db14b7873
Sasha Klein <sashafklein@gmail.com>
2021-06-28 22:06:20 -0400

    Smaller padding around the sign up form

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -18,30 +18,28 @@ export default () => {
         </div>
       </section>
 
-      <section className="section">
-        <div className="container columns">
-          <div className="content column is-8 is-offset-2">
-            <iframe
-              id="JotFormIFrame-211754736208053"
-              title="Alex Epstein - Email Subscribe Form - Simple "
-              onLoad={() => {
-                window.parent.scrollTo(0, 0);
-              }}
-              allowtransparency="true"
-              allowFullScreen={true}
-              allow="geolocation; microphone; camera"
-              src="https://form.jotform.com/211754736208053"
-              frameBorder="0"
-              style={{
-                minWidth: "100%",
-                height: "539px",
-                border: "none",
-              }}
-              scrolling="no"
-            ></iframe>
-          </div>
+      <div className="container columns">
+        <div className="content column is-8 is-offset-2">
+          <iframe
+            id="JotFormIFrame-211754736208053"
+            title="Alex Epstein - Email Subscribe Form - Simple "
+            onLoad={() => {
+              window.parent.scrollTo(0, 0);
+            }}
+            allowtransparency="true"
+            allowFullScreen={true}
+            allow="geolocation; microphone; camera"
+            src="https://form.jotform.com/211754736208053"
+            frameBorder="0"
+            style={{
+              minWidth: "100%",
+              height: "539px",
+              border: "none",
+            }}
+            scrolling="no"
+          ></iframe>
         </div>
-      </section>
+      </div>
     </Layout>
   );
 };
